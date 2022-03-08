2ba11d97308e3d29840a3685fa3d3cb296e14f31
Xing Wu <41498567+silverlobster@users.noreply.github.com>
2022-03-08 16:18:21 -0500

    basic sign up with validation created

--- signup.php
+++ signup.php
@@ -42,8 +42,8 @@
         <form id="signup" action="" method="post">
 
             <div id="signInField">
-                <label>Email:</label> <input type="text" name="email"><br>
-                <label>Password:</label> <input type="text" name="password"><br>
+                <label>Email:</label> <input type="text" name="email" required><br>
+                <label>Password:</label> <input type="text" name="password" required><br>
                 <input type="submit" name="signUp" value="Sign Up">
             </div>
         </form>
@@ -71,11 +71,55 @@
     session_start(); 
 
     if (isset($_POST['signUp'])) {
-        $email = $_POST['email'];
-        $password = $_POST['password'];
+        function validate($data) {
+            $data = trim($data);
+            $data = stripslashes($data);
+            $data = htmlspecialchars($data);
+            return $data;
+        }
+
+        $email = validate($_POST['email']);
+        $password = validate($_POST['password']);
+
+        $sql = "SELECT * FROM users WHERE username='$email'";
+        $result = mysqli_query($connect, $sql);
+
+        if (mysqli_num_rows($result) === 1) {
+            //$row = mysqli_fetch_assoc($result);
+            //if ($row['username'] === $email) {
+            echo "<br>" . "Email is already being used";
+        }
+            //}
+        else {
+            echo "<br>" . "Added to Database";
+            $sql = "INSERT INTO users (username, password) VALUES (?, ?)";
+            $stmt = $connect->prepare($sql);
+        
+            $stmt->bind_param("ss", $email, $password);
+            $stmt->execute();
+        }
+
+        $sql = "SELECT username, password FROM users";
+        $result = mysqli_query($connect, $sql);
+    
+        if ($result-> num_rows > 0) {
+            echo "<div align='center'><table><tr><th>Email</th><th>Password</th></th></tr>";
+            // output data of each row
+            while($row = $result->fetch_assoc()) {
+                echo "<tr><td>".$row["username"]."</td><td>  ".$row["password"] . "</td></tr>";
+            }
+            echo "</table></div>";
+        } else {
+            echo "0 results" . "<br>";
+        }
+
+        exit();
+
+        //echo $email . " " . $password;
     }
 
-    echo $email . " " . $password;
+
+
     
     /*
     if (isset($_POST['email']) && isset($_POST['password'])) {
