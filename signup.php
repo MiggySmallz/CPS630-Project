21410395a69552a39a568813dcbb3abb41dfe328
Miguel Nobre <mnobre24@gmail.com>
2022-03-13 15:58:51 -0400

    added autocomplete

--- signup.php
+++ signup.php
@@ -2,10 +2,38 @@
 <html lang="en">
     <meta charset="UTF-8">
 
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script defer
+        src="https://maps.googleapis.com/maps/api/js?libraries=places&language=en&key={Key_Here}"
+        type="text/javascript"></script>
+
     <head>
         <title>Smart Customer Services</title>
         <link rel="stylesheet" href="project-team19.css">
     </head>
+
+    <script>
+    $(function () {
+        var origin, destination, map;
+
+        // add input listeners
+        google.maps.event.addDomListener(window, 'load', function (listener) {
+            setDestination();
+        });
+
+        function setDestination() {
+            var from_places = new google.maps.places.Autocomplete(document.getElementById('address'));
+            
+
+            google.maps.event.addListener(from_places, 'place_changed', function () {
+                var from_place = from_places.getPlace();
+                var from_address = from_place.formatted_address;
+                $('#address').val(from_address);
+            });
+        }
+    });
+    </script>
+
 
     <body>
         <div class="menu-bar">
@@ -26,7 +54,7 @@
                 <label>Name:</label> <input type="text" name="name" required><br>
                 <label>Phone:</label> <input type="text" name="phone" required><br>
                 <label>Email:</label> <input type="text" name="email" required><br>
-                <label>Address:</label> <input type="text" name="address" required ><br>
+                <label>Address:</label> <input id="address" type="text" name="address" required ><br>
                 <label>Balance:</label> <input type="text" name="balance" required><br>
                 <input type="submit" name="signUp" value="Sign Up">
             </div>
