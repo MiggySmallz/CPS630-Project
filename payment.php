7801d98e77ff0289c54cc0a7995a1570c4deede7
Xing Wu <41498567+silverlobster@users.noreply.github.com>
2022-03-13 17:21:15 -0400

    added payment.php

---
+++ payment.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<meta charset="UTF-8">
+<title>Smart Customer Services</title>
+<link rel="stylesheet" href="project-team19.css">
+
+<style>
+</style>
+    <body>
+
+        <div class="menu-bar">
+            <div>
+                <a href="index.php"><img id="logo" src="logo.png"></a>
+            </div>
+            <div>
+                <a href="index.php">Home</a>
+            </div>
+            <div>
+                <a href="types_of_services.php">Types Of Services</a>   
+            </div>
+            <div>
+                <a href="reviews.php">Reviews</a>
+            </div>
+            <div>
+                <a href="shopping_cart.php">Shopping Cart</a>
+            </div>
+            <div>
+                <a href="about_us.php">About Us</a>
+            </div>
+            <div>
+                <a href="contact_us.php">Contact Us</a>
+            </div>
+            <div>
+                <a href="logout.php">Log Out</a>
+            </div>
+
+            <form id="signup" action="" method="post">
+
+                <div id="paymentField">
+                    <h2>Enter your Payment Info</h2>
+                    <label>Credit Card Number:</label> <input type="text" name="credit_card" required><br>
+                    <label>CVC</label> <input type="password" name="cvc" required><br>
+                    <input type="submit" name="save" value="Submit">
+                </div>
+            </form>
+        </div>
+    </body>
+</html>
+
+<?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "cps630";
+
+    $connect = mysqli_connect($servername, $username, $password);
+    $connect->query("CREATE DATABASE IF NOT EXISTS cps630;");
+    $connect->close();
+
+    function redirect($url, $permanent = false) {
+        if (headers_sent() === false) header('Location: ' . $url, true, ($permanent === true) ? 301 : 302);
+        exit();
+    }
+
+    if (isset($_POST['save'])) {
+        redirect("thank_you.php");
+    }
+?>
+
